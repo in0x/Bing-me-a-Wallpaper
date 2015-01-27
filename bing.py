d7bb7bf9d2778c55f57dfe16562fae2bbd3c55bd
Philipp Welsch <pwelsch86@gmail.com>
2015-01-27 17:29:42 +0100

    Initial Commit

---
+++ bing.py
@@ -0,0 +1,49 @@
+import urllib
+import urllib2
+import os
+import getpass
+
+#get the current login name
+userName = getpass.getuser()
+
+bingSave = True;
+#check if the user already has a wallpaper dir, if yes make a "bing" one
+#if not make one
+if os.path.exists("/Users/" + userName + "/Pictures/wallpapers") :
+	os.makedirs("/Users/" + userName + "/Pictures/BingWallpapers")
+	bingSave = True
+elif not os.path.exists("/Users/" + userName + "/Pictures/wallpapers") :
+	os.makedirs("/Users/" + userName + "/Pictures/wallpapers")
+	bingSave = False
+
+#Get the html code from bing
+httpString = urllib2.urlopen("http://www.bing.com/#").read()
+
+url = ""
+
+#build the relative url of the image, assuming they dont 
+#change the directory on me (cheeky bing people)
+for x in range(1, 56) :
+	url += httpString[45489 + x]
+
+#change the desired resolution of the image
+url = url.replace("1366x768", "1920x1200")
+
+#build a total url to download the file
+totalURL = "http://www.bing.com/az/" + url
+
+#extract the image name for saving
+imageName = url[12:]
+
+#change to image directory for saving
+if not (bingSave) :
+	directory = ("/Users/" + userName + "/Pictures/wallpapers")
+	os.chdir(directory)
+elif (bingSave) :
+	directory = ("/Users/" + userName + "/Pictures/BingWallpapers")
+	os.chdir(directory)
+
+#create a connection instance and download the image + save it
+imageInstance = urllib.URLopener()
+imageInstance.retrieve(totalURL, imageName) 
+urllib.urlcleanup()
---
+++ bing.sh
@@ -0,0 +1,16 @@
+echo "Enter how often you'd like to check for new wallpapers"
+echo
+echo "for example, for everyday at 11 write * 11 * * *"
+echo
+echo "for every 50 minutes you could write 50 * * * *"
+echo
+echo "then enter the path where bing.py is stored (you can drag and drop into terminal)"
+echo
+echo "press control + o to accept your entry and control + x to exit"
+echo
+echo "press any key to continue"
+read -n 1 -s
+env EDITOR=nano crontab -e
+
+echo "if everything worked you should see crontab: installing new crontab"
+echo "if not, you can manually write the cronjob by entering env EDITOR=nano crontab -e"
