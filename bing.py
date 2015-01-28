1d5dc6ce61807e50f237338a764f144dcde93155
Philipp Welsch <pwelsch86@gmail.com>
2015-01-28 11:44:43 +0100

    Adjusted how urls are built

--- bing.py
+++ bing.py
@@ -22,24 +22,37 @@ elif not os.path.exists("/Users/" + userName + "/Pictures/wallpapers") :
 
 #Get the html code from bing
 httpString = urllib2.urlopen("http://www.bing.com/#").read()
-
+httpOut = open('http.txt', 'w')
+httpOut.write(httpString)
 url = ""
 
 #build the relative url of the image, assuming they dont 
 #change the directory on me (cheeky bing people)
-for x in range(1, 1000) :
+#img={url:
+#use string.find()
+
+beginIndex = httpString.find("img={url:", 10000)
+
+"""for x in range(1, 1000) :
 	if(httpString[45489 + x] == "'") :
 		break 
-	url += httpString[45489 + x]
+	url += httpString[45489 + x]"""
+
+for x in range(beginIndex + len("img={url:") + 2, beginIndex + 1000) :
+	if(httpString[x] == "'") :
+		break
+	url += httpString[x]
 
 #change the desired resolution of the image
+print(url)
 url = url.replace("1366x768", "1920x1200")
 
 #build a total url to download the file
-totalURL = "http://www.bing.com/az/" + url
+totalURL = "http://www.bing.com/" + url
 
 #extract the image name for saving
-imageName = url[12:]
+imageName = url[15:]
+print(imageName)
 
 #change to image directory for saving
 if not (bingSave) :
