073d109a26d18458d038660cb4147b4d770ac82a
Philipp Welsch <pwelsch86@gmail.com>
2015-07-02 23:57:48 +0200

    Update bing.py

--- bing.py
+++ bing.py
@@ -3,41 +3,57 @@ import os
 import getpass
 import re
 
-#get the current login name
-userName = getpass.getuser()
-picuteRoot = "/Users/" + userName + "/Pictures/"
-savePath = picuteRoot + "BingWallpapers"
+def main():
+	#get the current login name
+	userName = getpass.getuser()
+	picuteRoot = "/Users/" + userName + "/Pictures/"
+	savePath = picuteRoot + "BingWallpapers"
 
-#check if the user already has a wallpaper dir, if yes make a "bing" one
-#if not make one
-if not os.path.exists(savePath) :
-	if not os.path.exists(picuteRoot + "wallpapers") :
-		savePath = picuteRoot + "wallpapers"
-	os.makedirs(savePath)
+	#check if the user already has a wallpaper dir, if yes make a "bing" one
+	#if not make one
+	if not os.path.exists(savePath) :
+		if not os.path.exists(picuteRoot + "wallpapers") :
+			savePath = picuteRoot + "wallpapers"
+		os.makedirs(savePath)
 
-#Get the html code from bing
-htmlString = urllib2.urlopen("http://www.bing.com/").read()
+	#Get the html code from bing
+	try:
+		htmlString = urllib2.urlopen("http://www.bing.com/").read()
+	except Exception, e:
+		print str(e) + " - Could not read from bing.com (No connection)"
+		return
 
-#build the relative url of the image, assuming they dont 
-#change the directory on me (cheeky bing people)
-urlRe = re.search("g_img={url:'(.*?)\.jpg'", htmlString)
-url = urlRe.group(1) + ".jpg"
+	#build the relative url of the image, assuming they dont 
+	#change the directory on me (cheeky bing people)
+	urlRe = re.search("g_img={url:'(.*?)\.jpg'", htmlString)
+	url = urlRe.group(1) + ".jpg"
 
-#change the desired resolution of the image
-url = url.replace("1366x768", "1920x1200")
+	#change the desired resolution of the image
+	url = url.replace("1366x768", "1920x1200")
 
-#build a total url to download the file
-totalURL = "http://www.bing.com" + url
+	#build a total url to download the file
+	totalURL = "http://www.bing.com" + url
 
-imageName = savePath + "/" + url.split('/')[-1]
-print("saving image to: " + imageName)
+	imageName = savePath + "/" + url.split('/')[-1]
 
-#create a connection instance and download the image + save it
-imageFile = urllib2.urlopen(totalURL)
-imageOut = open(imageName, 'wb')
-imageOut.write(imageFile.read())
-imageOut.close()
+	if os.path.exists(imageName):
+		print "Image already exists"
+		return
 
-text_file = open("dir.txt", 'w')
-text_file.write(imageName + "\n")
-text_file.close()
+	print("saving image to: " + imageName)
+
+	#create a connection instance and download the image + save it
+	try:
+		imageFile = urllib2.urlopen(totalURL)
+		imageOut = open(imageName, 'wb')
+		imageOut.write(imageFile.read())
+		imageOut.close()
+	except Exception, e:
+		print str(e) + " - failed to load image"
+
+	text_file = open("dir.txt", 'w')
+	text_file.write(imageName + "\n")
+	text_file.close()
+	print("Successful")
+
+main()
